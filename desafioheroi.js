85f2327072b386f1748b94fc5cc21aa35a8d41ac
web3shark <pramos08@hotmail.com>
2025-07-23 18:55:25 -0300

    Desafio Heroi WebDio

---
+++ desafioheroi.js
@@ -0,0 +1,42 @@
+
+let nomeHeroi = "Superman";
+let expHeroi = 1001; // Defina aqui o valor de experiência do herói
+
+
+if (expHeroi <= 1000) {
+    console.log(" O herói " + nomeHeroi + " está no nível Ferro");
+
+}
+
+else if (expHeroi > 1000 && expHeroi < 2001) {
+    console.log(" O herói " + nomeHeroi + " está no nível Bronze");
+
+}
+
+else if (expHeroi >= 2001 && expHeroi < 5001) {
+    console.log(" O herói " + nomeHeroi + " está no nível Prata");
+
+}
+
+else if (expHeroi >= 5001 && expHeroi < 7001) {
+    console.log(" O herói " + nomeHeroi + " está no nível Ouro");
+
+}
+
+else if (expHeroi >= 7001 && expHeroi < 8001) {
+    console.log(" O herói " + nomeHeroi + " está no nível Platina");
+
+} 
+
+else if (expHeroi >= 8001 && expHeroi < 9001) {
+    console.log(" O herói " + nomeHeroi + " está no nível Ascendente");
+} 
+
+else if (expHeroi >= 9001 && expHeroi < 10001) {
+    console.log(" O herói " + nomeHeroi + " está no nível Imortal");
+} 
+
+else if (expHeroi >= 10001) {
+    console.log(" O herói " + nomeHeroi + " está no nível Radiante");
+}
+
